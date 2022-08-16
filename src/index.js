82a715aac958006f204aff8ab1e5800a6ce3c9ab
Loltolo-Lesapiti <petrolesapiti@gmail.com>
2022-08-16 16:27:54 +0300

    Update script index file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import './icon.png'
+import './style.css'
 import addTask from "./module/add.js"
 import {taskList,userInput,clearBtn} from "./module/variables.js"
 import PopEditandRemove from "./module/DisplayAddandRemove.js"
@@ -10,15 +12,9 @@ Pop.display();
 const dots= document.querySelectorAll("#dots");
 const edit= document.querySelectorAll("#edit");
 const del= document.querySelectorAll("#del");
-dots.forEach(dot=>{
-    dot.addEventListener("click",()=>Pop.popUp(dot));
-})
-edit.forEach(item=>{
-    item.addEventListener("click",()=>Pop.editTask(userInput.value));
-})
-del.forEach(item=>{
-    item.addEventListener("click",()=>Pop.removeTask(item));
-})
+dots.forEach(dot=>dot.addEventListener("click",()=>Pop.popUp(dot)));
+edit.forEach(item=>item.addEventListener("click",()=>Pop.editTask(userInput.value)));
+del.forEach(item=>item.addEventListener("click",()=>Pop.removeTask(item)));
 clearBtn.addEventListener('click', () =>removeAll());
 
 
